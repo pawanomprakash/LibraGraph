163a6ee009e00dd361f7a6b74e424c8c6dfb578a
PRABHAS VARMA <prabhasvarma017@gmail.com>
2024-08-23 14:38:34 +0530

    created basic frontend

---
+++ Client/LibraGraph/src/components/LibraBot.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LibraBot = () => {
+  return (
+    <div className="flex gap-10 text-4xl font-extrabold text-center whitespace-nowrap">
+      <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/9a11ab41bac00c6a11f65915cf18aafcea81b2454728a7aaa26e245014614074?placeholderIfAbsent=true&apiKey=b4ed591bb3324416897d3c9c8b1b99c2" alt="LibraBot logo" className="object-contain shrink-0 aspect-[1.21] w-[70px]" />
+      <div className="self-start basis-auto">LibraBot</div>
+    </div>
+  );
+};
+
+export default LibraBot;
