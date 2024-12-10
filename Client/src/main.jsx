7033d1daa61037e293c648ec35fbd6d98cadf623
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-12-10 10:38:48 +0530

    added routing

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
@@ -12,8 +12,10 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <BrowserRouter>
    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <App />
     </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
